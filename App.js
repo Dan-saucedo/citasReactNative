36fa84ad1e12e466f6d3e3f3cf27bcdd4a04d221
Daniel Saucedo Avila <st4587@utr.edu.mx>
2026-09-23 12:11:51 -0600

    Packages installation and configurations

--- App.js
+++ App.js
@@ -1,31 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Pressable } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>¡Hola, Mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style = {styles.container}>
+      <SafeAreaView>
+          <Text style = {styles.titulo}> Administrador de citas {''}
+            <Text style = {styles.tituloBold}> Veterinaria </Text>
+          </Text>
+
+          <Pressable style = {styles.btnNuevaCita}>
+            <Text style = {styles.btnTextNuevaCita}> Nueva cita </Text>
+          </Pressable>
+
+      </SafeAreaView>
+      <StatusBar style="auto"/>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#f3f4f6',
     flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#333333',
-    marginBottom: 8,
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
   },
-  subtitle: {
-    fontSize: 16,
-    color: '#666666',
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6d28d9'
   },
-});
+  btnNuevaCita: {
+    backgroundColor: '#6d28d9',
+    padding: 15,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10
+  },
+  btnTextNuevaCita: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 18,
+
+  }
+})
